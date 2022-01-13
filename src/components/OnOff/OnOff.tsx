a13e6add90f95fcad66960986af581c6d87390e1
Dzmitry <maddal.privod@gmail.com>
2022-01-13 05:06:45 +0300

    create buttons On/Off

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,40 @@
+type PropsType = {
+    on: boolean
+}
+
+
+export function OnOff(props:PropsType) {
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        background: props.on ? 'green' : 'white'
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        background: !props.on ? 'red' : 'white',
+        marginLeft: '2px'
+    }
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        background: props.on ? 'green' : 'red'
+    }
+    return (
+    <div>
+        <div style={onStyle}>On</div>
+        <div style={offStyle}>Off</div>
+        <div style={indicatorStyle}></div>
+    </div>
+    )
+}
