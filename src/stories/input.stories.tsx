59d66a9441ddf35d3d2294f037ba7cd62521c071
Dzmitry <maddal.privod@gmail.com>
2022-02-06 17:32:00 +0300

    created UncontrolledInput

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
+import {OnOff} from "../components/OnOff/OnOff";
+
+
+export default {
+    title: 'input',
+    //component: OnOff
+}
+
+export const UncontrolledInput = () => <input/>;
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const actualValue = e.currentTarget.value
+        setValue(actualValue)
+    }
+    return <><input onChange={onChange}/> - {value}</>
+};
+
+export const GetValueUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <><input ref={inputRef}/>
+        <button onClick={save}>save</button>
+        -actual value: {value}</>
+};
+
+export const ControlledInput = () => <input value={'it-incubator'}/>;
